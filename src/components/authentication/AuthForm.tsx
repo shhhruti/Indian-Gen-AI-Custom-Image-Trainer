dc2385ece4d8d0c1e44bc44d04538d3ade324164
addygeek <ui22cs03@iiitsurat.ac.in>
2025-02-11 23:50:23 +0530

    auth page

--- src/components/authentication/AuthForm.tsx
+++ src/components/authentication/AuthForm.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 const AuthForm = () => {
+  const [mode, setMode] = useState('login'); // 'login', 'signup', or 'reset'
   return (
-    <div>AuthForm
-      <p> Enter your email: </p>
+    <div className="space-y-6">
+      <div className="flex flex-col space-y-2 text-center">
+        <h1 className="text-2xl font-semibold tracking-tight">
+          {mode === 'reset'
+            ? 'Reset Password'
+            : mode === 'login'
+            ? 'Login'
+            : 'Sign Up'}
+        </h1>
+        <p className="text-sm text-muted-foreground">
+          {mode === 'reset'
+            ? 'Enter your email below to reset your password'
+            : mode === 'login'
+            ? 'Enter your email below to login to your account'
+            : 'Enter your information below to create an account'}
+        </p>
+      </div>
+      {
+        mode === 'login' && <span> LoginForm </span>
+      }
+       {
+        mode === 'signup' && <span> SignupForm </span>
+      }
+       {
+        mode === 'reset' && <span> Reset passowrd form </span>
+      }
     </div>
   )
 }
---
+++ src/components/authentication/LoginForm.tsx
@@ -0,0 +1,73 @@
+"use client"
+import React from 'react'
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Pleae enter a valid email address"
+  }),
+  password: z.string().min(8,
+    {
+      message: "Please enter a valid password"
+    }
+  )
+})
+
+function LoginForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+
+  return (
+    <div className={cn("grid gap-6"), className}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
+
+export default LoginForm
