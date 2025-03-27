8bb246e87c9c4a002d643296557bbe54aab24d68
addygeek <ui22cs03@iiitsurat.ac.in>
2025-03-27 23:06:06 +0530

    galery setup

--- src/app/(dashboard)/gallery/page.tsx
+++ src/app/(dashboard)/gallery/page.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
 
-function Gallery() {
+import { getImages } from '@/app/actions/image-acations';
+import GalleryComponent from '@/components/gallery/GalleryComponent';
+
+import React from 'react'
+// import GalleryComponent from '@/components/gallery/GalleryComponent';
+
+const Gallery = async () => {
+
+  const {data: images} = await getImages;
   return (
-    <div>Gallery Page</div>
+    <section className='container mx-auto'>
+      <h1 className='text-3xl font-semibold mb-2'>My Images</h1>
+      <p className='text-muted-foreground mb-6'>
+        Here you can see all the images you have generated. Click on an image to view details. 
+      </p>
+      <GalleryComponent images={images || []} />
+    </section>
   )
 }
 
--- src/app/actions/image-acations.ts
+++ src/app/actions/image-acations.ts
@@ -1,14 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use server'
 import { z } from "zod"
-//import { ImageGenerationFormSchema } from "@/components/image-generation/Configurations";
-import Replicate from "replicate";
-// import { createClient } from "@/lib/supabase/server";
-// import { Database } from '@datatypes.types';
-// import { imageMeta } from "image-meta";
-// import { randomUUID } from "crypto";
-// import { getCredits } from "./credit-actions";
 
+import Replicate from "replicate";
+import { createClient } from "@/lib/supabase/server";
+//import { Database } from '@datatypes.types';
+import { imageMeta } from "image-meta";
+import { randomUUID } from "crypto";
+ import { getCredits } from "./credit-actions";
+import { ImageGenerationFormSchema } from '@/components/image-generation/Configurations';
+import { Database } from "../../../database.types";
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
@@ -82,4 +83,170 @@ export async function generateImageAction(input: (z.infer<typeof ImageGeneration
             data: null
         }
       }
+}
+
+export async function imgUrlToBlob(url:string){
+    const response = fetch(url);
+    const blob = (await response).blob();
+    return (await blob).arrayBuffer();
+}
+
+type storeImageInput = {url: string} & Database["public"]["Tables"]["generated_images"]["Insert"]
+
+
+export async function storeImages(data: storeImageInput[]){
+    const supabase = await createClient()
+
+    const {data: {user}} = await supabase.auth.getUser();
+    if(!user){
+        return {
+            error: 'Unauthorized',
+            success: false,
+            data: null,
+        }
+    }
+
+    const uploadResults = [];
+
+    for(const img of data){
+        const arrayBuffer = await imgUrlToBlob(img.url);
+         const {width, height, type} = imageMeta(new Uint8Array(arrayBuffer));
+
+         const fileName = `image_${randomUUID()}.${type}`
+         const filePath = `${user.id}/${fileName}`
+
+         const {error: storageError} = await supabase.storage.from('generated_images').upload(
+            filePath, arrayBuffer, {
+                contentType: `image/${type}`, // image/png or image/jp
+                cacheControl: '3600',
+                upsert: false,
+            }
+         )
+         
+         if(storageError){
+            uploadResults.push({
+                fileName,
+                error: storageError.message,
+                success: false,
+                data: null,
+            })
+            continue;
+         }
+
+         const {data:dbData, error: dbError} = await supabase.from('generated_images').insert([{
+            user_id: user.id,
+            model: img.model,
+            prompt: img.prompt,
+            aspect_ratio: img.aspect_ratio,
+            guidance: img.guidance,
+            num_inference_steps: img.num_inference_steps,
+            output_format: img.output_format,
+            image_name: fileName,
+            width,
+            height
+         }]).select()
+         
+
+         if(dbError){
+            uploadResults.push({
+                fileName,
+                error: dbError.message,
+                success: !dbError,
+                data: dbData || null,
+            })
+         }
+    }
+
+
+    return {
+        error: null,
+        success: true,
+        data: {results: uploadResults}
+     }
+
+
+
+}
+
+export async function getImages(limit?: number){
+    const supabase = await createClient()
+
+    const {data: {user}} = await supabase.auth.getUser();
+    if(!user){
+        return {
+            error: 'Unauthorized',
+            success: false,
+            data: null,
+        }
+    }
+
+    let query = supabase.from("generated_images").select("*").eq("user_id", user.id).order("created_at", {ascending: false})
+
+    if(limit){
+        query = query.limit(limit)
+    }
+
+    const {data, error} = await query;
+
+    if(error){
+        return {
+            error: error.message || "Failed to fetch images!",
+            success: false,
+            data:null
+        }
+    }
+
+    const imageWithUrls = await Promise.all(
+        data.map(async (image: Database["public"]["Tables"]["generated_images"]["Row"]) => {
+            const { data } = await supabase.storage
+  .from('generated_images')
+  .createSignedUrl(`${user.id}/${image.image_name}`, 3600)
+
+  return {
+    ...image,
+    url: data?.signedUrl
+  }
+        })
+    )
+
+
+  
+    return {
+        error: null,
+        success: true,
+        data: imageWithUrls || null
+     }
+
+
+
+}
+
+
+export async function deleteImage(id: string, imageName:string){
+    const supabase = await createClient()
+
+    const {data: {user}} = await supabase.auth.getUser();
+    if(!user){
+        return {
+            error: 'Unauthorized',
+            success: false,
+            data: null,
+        }
+    }
+
+  const {data, error} =   await supabase.from('generated_images').delete().eq('id', id);
+  if(error){
+    return {
+        error: error.message, success:false, data:null
+    }
+  }
+
+  await supabase.storage.from('generated_images').remove([`${user.id}/${imageName}`])
+
+
+
+  return {
+    error: null, success: true, data: data
+  }
+
 }
